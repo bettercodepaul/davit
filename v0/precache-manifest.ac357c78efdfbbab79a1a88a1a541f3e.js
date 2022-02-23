5f3bfcd8d603f413ecfc354ecedbbd1e307417b7
exx-Andreas <exx-Andreas@users.noreply.github.com>
2022-02-23 09:24:05 +0000

    Deploying to gh-pages from  @ 37c583d2f103a1af0b714f3ca374ac599ed61b07 🚀

---
+++ v0/precache-manifest.ac357c78efdfbbab79a1a88a1a541f3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95861495f89e52797460af6814aad978",
+    "url": "/davit/v0/index.html"
+  },
+  {
+    "revision": "4d885bfea2cb6c97d523",
+    "url": "/davit/v0/static/css/main.07a22398.chunk.css"
+  },
+  {
+    "revision": "68a7c3ff2e4dd909a593",
+    "url": "/davit/v0/static/js/2.f9206e1f.chunk.js"
+  },
+  {
+    "revision": "f4740de751db22a2ef1fd28a4622ec2b",
+    "url": "/davit/v0/static/js/2.f9206e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d885bfea2cb6c97d523",
+    "url": "/davit/v0/static/js/main.e5593197.chunk.js"
+  },
+  {
+    "revision": "7aecd1df95bdc74649ab",
+    "url": "/davit/v0/static/js/runtime-main.cbd70936.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/davit/v0/static/media/DINOT-Bold.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/davit/v0/static/media/DINOT-Light.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/davit/v0/static/media/DINOT-Medium.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d20fbb57af21da9535616716b4dc464e",
+    "url": "/davit/v0/static/media/exxcellent_logo_200.d20fbb57.png"
+  }
+]);
